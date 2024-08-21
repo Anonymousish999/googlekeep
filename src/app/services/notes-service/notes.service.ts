7b2c764e31548b1b518e059558b520411e2a4f98
Ishu Gupta <guptarhsu999@gmail.com>
2024-08-21 14:20:43 +0530

    Add: archive and trash feature added

--- src/app/services/notes-service/notes.service.ts
+++ src/app/services/notes-service/notes.service.ts
@@ -13,9 +13,16 @@ export class NotesService {
     );
   }
 
-  addNotesApiCall(url:any, data: any) {
+  addNotesApiCall(url: any, data: any) {
     return this.httpService.postAPIcall(
       `${url}?access_token=${this.access_token}`,
+      data
+    );
+  }
+
+  noteIconsApiCall(data: any, endpoint: string) {
+    return this.httpService.postAPIcall(
+      `/notes${endpoint}?access_token=${this.access_token}`,
       data
     );
   }
